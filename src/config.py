7e89a37f5b2806d1af8110e4910ab9a4c01edced
Wojciech Adamski <wwadamski@gmail.com>
2023-09-24 22:46:21 +0200

    niebezpieiczny commit, ZMIEŃ KLUCZE

---
+++ src/config.py
@@ -0,0 +1,9 @@
+SQL_ADMIN_CREDENCIALS = ['adm123453324@gmaill.com', 'admin']
+SQL_USER_CREDENTIALS = ['user@gmail.com', 'user']
+ALLOWED_EXTENSIONS = {'csv', 'json'}
+UPLOAD_FOLDER = 'uploads'
+EMAIL_KEY = b'ZmDfcTF7_60GrrY167zsiPd67pEvs0aGOv2oasOM1Pg='
+class Config(object):
+    SECRET_KEY = 'RBrOvJOwFasCw0DXazq7BNB2VST5c38B'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
+
